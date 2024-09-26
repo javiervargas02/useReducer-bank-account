04d74f35e4c1aa05f0895ae5a6d4d150a6e25903
javiervargas02 <javier.v.021204@gmail.com>
2024-09-26 16:08:04 -0600

    Refactor reducer to handle input of deposit, withdrawal, and loan amounts

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -2,26 +2,53 @@ import toast from "react-hot-toast";
 
 export const reducer = (state, action) => {
   toast.dismiss();
-  const { balance, loan } = state;
+  const { balance, loan, withdrawAmount, depositAmount, loanAmount } = state;
   switch (action.type) {
     case "open":
       toast.success("Account opened");
       return { ...state, account: "open", balance: 500 };
     case "deposit":
-      return { ...state, balance: balance + 100 };
+      return { ...state, balance: balance + depositAmount };
     case "withdraw":
       return {
         ...state,
-        balance: balance >= 50 ? balance - 50 : balance,
+        balance: balance >= withdrawAmount ? balance - withdrawAmount : balance,
       };
     case "loan":
       return {
         ...state,
-        loan: loan === 5000 ? loan : loan + 5000,
-        balance: balance + 5000,
+        loan: loan === loanAmount ? loan : loan + loanAmount,
+        balance: balance + loanAmount,
       };
     case "pay":
-      return { ...state, loan: 0, balance: balance - 5000 };
+      return { ...state, loan: 0, balance: balance - loan };
+    case "setDepositAmount":
+      if (isNaN(action.payload) || action.payload <= 0) {
+        return {
+          ...state,
+          depositAmount: action.payload,
+          depositError: "Invalid amount",
+        };
+      }
+      return { ...state, depositAmount: action.payload, depositError: "" };
+    case "setWithdrawAmount":
+      if (isNaN(action.payload) || action.payload <= 0) {
+        return {
+          ...state,
+          withdrawAmount: action.payload,
+          withdrawError: "Invalid amount",
+        };
+      }
+      return { ...state, withdrawAmount: action.payload, withdrawError: "" };
+    case "setLoanAmount":
+      if (isNaN(action.payload) || action.payload <= 0) {
+        return {
+          ...state,
+          loanAmount: action.payload,
+          loanError: "Invalid amount",
+        };
+      }
+      return { ...state, loanAmount: action.payload, loanError: "" };
     case "close":
       if (loan !== 0) {
         toast.error("Loan must be payed to close account");
@@ -29,9 +56,8 @@ export const reducer = (state, action) => {
       } else if (balance !== 0) {
         toast.error("Balance must be 0 to close account");
         return state;
-      } else {
-        return { ...state, account: "closed" };
       }
+      return { ...state, account: "closed" };
     default:
       return state;
   }
