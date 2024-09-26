576fa80d20a6e0a76672320ce8490bd8b3cf932e
javiervargas02 <javier.v.021204@gmail.com>
2024-09-26 14:56:08 -0600

    Add Button component to dispatch the indicated action

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+export default function Button({ children, disabled = false, dispatch }) {
+  return (
+    <button
+      disabled={disabled}
+      onClick={() => dispatch()}
+      className="bg-[#7f00ff] enabled:hover:bg-[#984cf0] text-white w-full py-3 rounded-xl shadow-lg duration-200 disabled:opacity-50 disabled:cursor-not-allowed">
+      {children}
+    </button>
+  );
+}
