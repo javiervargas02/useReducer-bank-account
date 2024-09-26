a07ee142000b53913dacdaae29ca3d90238fbce6
javiervargas02 <javier.v.021204@gmail.com>
2024-09-26 14:56:26 -0600

    Add Header component with title and subtitle

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="space-y-4">
+      <h1 className="text-5xl md:text-6xl font-semibold text-center text-white">
+        Bank Account
+      </h1>
+      <h2 className="text-white text-3xl md:text-4xl font-medium text-center">
+        useReducer Hook
+      </h2>
+    </header>
+  );
+}
